5227e80d40289daab6d1afcba71fa868f89b6ebf
PCampello <pcampello19@gmail.com>
2019-03-26 02:12:47 -0400

    I stopped on #19

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,19 @@
 export const createProject = (project) => {
 	return (dispatch, getState, { getFirebase, getFirestore }) => {
-		// make async call to database	
-		dispatch({ type: 'CREATE_PROJECT', project })
+		// make async call to database
+		const firestore = getFirestore();
+		firestore.collection('projects').add({ 
+			...project,
+			authorFirstName: 'Donald',
+			authorLastName: 'Trump',
+			authorId: 12345,
+			createdAt: new Date()
+		}).then(() => {
+			dispatch({ type: 'CREATE_PROJECT', project });	
+		}).catch((err) => {
+			dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+		})
+		
 
 	}
 }
