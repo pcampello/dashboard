cc53e017f625c2c1dde85db0bdffead1972aa3ae
PCampello <pcampello19@gmail.com>
2019-04-19 17:58:11 -0400

    Project Ready to Run

---
+++ dist/precache-manifest.f717ce0639d09b62d3e0b0d6b2749da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cab89cd6008ceb044bb3",
+    "url": "/static/js/main.3ba0f755.chunk.js"
+  },
+  {
+    "revision": "66ee83eb64318d3e1f05",
+    "url": "/static/js/2.325be6f4.chunk.js"
+  },
+  {
+    "revision": "cab89cd6008ceb044bb3",
+    "url": "/static/css/main.322091c3.chunk.css"
+  },
+  {
+    "revision": "7c3b95820c4512d68d4f34640fb7a060",
+    "url": "/index.html"
+  }
+];
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,44 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello nijas!");
+  response.send("Hello ninjas!");
+});
+
+const createNotification = notification => {
+  const doc = admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+}
+
+exports.projectCreated = functions.firestore
+.document('projects/{projectId}')
+.onCreate(doc => {
+
+  const project = doc.data();
+  const notification = {
+    content: 'Added a new project',
+    user: `${project.authorFirstName} ${project.authorLastName}`,
+    time: admin.firestore.FieldValue.serverTimestamp()
+  }
+
+  return createNotification(notification);
+
+});
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+
+        }
+        return createNotification(notification);
+      })
 });
